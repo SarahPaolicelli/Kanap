a9ca65ec315cc512a077c8537efc3ad146beb15e
SarahPaolicelli <sarahpaolicelli@gmail.com>
2022-11-04 12:43:01 +0100

    Ajout page product et debut page cart

--- front/js/product.js
+++ front/js/product.js
@@ -24,3 +24,48 @@ function showResults(data){
     }
 }
 
+                                //PANIER//
+
+
+//Verifie si il y a deja qqch dans le panier//
+function getBasket(){
+    let basket=localStorage.getItem("basket")
+    if (basket==null){
+        return[]
+    } 
+    else{
+        return JSON.parse(basket)
+    }
+}
+
+
+//mémorise le panier dans le localstorage//
+function saveBasket(basket){
+    localStorage.setItem("basket",JSON.stringify(basket))
+}
+
+
+//recherche si le produit est deja dans le panier 
+function addBasket(product){
+    let basket=getBasket()
+    let foundProduct=basket.find((p)=>p.id==product.id&&p.color==product.color)
+    if (foundProduct==null){
+        basket.push(product)
+        saveBasket(basket)
+    }
+    else{
+        foundProduct.quantity+=product.quantity
+        saveBasket(basket)
+    }
+}
+
+//déclare les variables à prendre en compte pour l'ajout au panier au clique de la souris sur le bouton "addToCart"//
+addToCart.onclick=()=>{
+    let product={
+        id:idValue,
+        color:colors.value,
+        quantity:parseInt(quantity.value)
+    }
+    addBasket(product)
+    alert('Votre produit a bien été ajouté à votre panier')
+}

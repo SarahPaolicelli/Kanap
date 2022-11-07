049a971da47a1e86af58f33ac05fecd2125f6c17
SarahPaolicelli <sarahpaolicelli@gmail.com>
2022-11-07 12:17:01 +0100

    Ajout vérification données qty et color

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,18 @@
 //Récuperation de l'id du produit à partir de l'URL de la page //
-let idValueFull=window.location.search
-let urlParams=new URLSearchParams(idValueFull)
+let urlParameter=window.location.search
+let urlParams=new URLSearchParams(urlParameter)
 let idValue=urlParams.get('_id')
 
 
 //fetch recupere uniquement le produit dont l'id correspond à l'URL //
 fetch("http://localhost:3000/api/products/"+idValue)
     .then(response=>response.json())
-    .then(data=>{showResults(data)})   
+    .then(data=>{showResult(data)})   
     .catch(err=>console.log("Il y'a une erreur : "+err+" , il faut corriger l'erreur et réessayer."))
 
 
 //fonction qui affiche les résultats dans les champs correspondants //
-function showResults(data){
+function showResult(data){
     document.querySelector(".item__img").insertAdjacentHTML("afterbegin",`<img src="${data.imageUrl}" alt="${data.altTxt}">`)
     document.getElementById("price").insertAdjacentHTML("afterbegin", `${data.price}`)
     document.getElementById("description").insertAdjacentHTML("afterbegin",`${data.description}`)
@@ -24,8 +24,8 @@ function showResults(data){
     }
 }
 
-                                //PANIER//
 
+//PANIER//
 
 //Verifie si il y a deja qqch dans le panier//
 function getBasket(){
@@ -60,12 +60,21 @@ function addBasket(product){
 }
 
 //déclare les variables à prendre en compte pour l'ajout au panier au clique de la souris sur le bouton "addToCart"//
+//Vérifie que les données ont bien été entrées
 addToCart.onclick=()=>{
     let product={
         id:idValue,
         color:colors.value,
         quantity:parseInt(quantity.value)
     }
-    addBasket(product)
-    alert('Votre produit a bien été ajouté à votre panier')
+    if (product.quantity<1 || product.quantity>100){
+        alert('veuillez entrer une quantité comprise entre 1 et 100')
+    }   
+    else if(product.color==0){
+        alert('veuillez choisir une couleur dans la liste')
+    }
+    else{
+        addBasket(product)
+        alert('Votre produit a bien été ajouté à votre panier')
+    }          
 }
